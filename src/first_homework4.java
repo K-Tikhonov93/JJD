80e787340fc89ed90c3fcc3245f3bda62f1cb465
Konstantin <tikhonoff93@gmail.com>
2017-10-10 12:09:12 +0300

    Домашнее задание №4

---
+++ src/first_homework4.java
@@ -0,0 +1,10 @@
+public class first_homework4 {
+    public static void main(String[] args) {
+        int a = 284;
+        int b = a%10;
+        int c = (a/10)%10;
+        int d = (a/100)%10;
+        System.out.println("Если заданное число "+a+" значит это число состоит из цифр "+ d +" "+ c +" и "+ b +" и их сумма равна "+(b+c+d));
+
+    }
+}
