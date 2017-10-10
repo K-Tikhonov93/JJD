29e483d93615aacdee19770d84c989635814adc0
Konstantin <tikhonoff93@gmail.com>
2017-10-10 12:05:10 +0300

    Домашнее задание №2

---
+++ src/first_homework2.java
@@ -0,0 +1,9 @@
+public class first_homework2 {
+    public static void main(String[] args) {
+        int a = 28;
+        int b = a%10;
+        int c = (a/10)%10;
+        System.out.println("Если заданное число "+a+" значит это число состоит из цифр "+c+" и "+b+" и их сумма равна "+(b+c));
+
+    }
+}
