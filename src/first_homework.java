d643acf7ba5a9f4faaa503ce12fffde761944b3b
Konstantin <tikhonoff93@gmail.com>
2017-10-10 11:37:57 +0300

    Домашнее задание №1

---
+++ src/first_homework.java
@@ -0,0 +1,8 @@
+public class first_homework {
+    public static void main(String[] args) {
+        int q = 21;
+        int w = 8;
+
+        System.out.println("Результат деления "+q+" на "+w+" равняется "+q/w+" и остаток "+q%w);
+    }
+}
