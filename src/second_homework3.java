d71c794fc207239580fc7bef2520801fc1f3f142
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-13 20:58:48 +0300

    Урок второй Домашнее задание №3

---
+++ src/second_homework3.java
@@ -0,0 +1,25 @@
+public class second_homework3 {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = 4;
+        double c = 1;
+
+        System.out.println("Необходимо найти корни квадратного уравнения типа ax^2+bx+c=0");
+
+        double d = (b*b)-(4*a*c);
+        double i;
+        double j;
+
+        if (d==0){
+            i = (-1*b)/(2*a);
+            System.out.println("Уравнение имеет один корень и равен "+ i);
+        }
+        else if (d>0){
+            i = ((-1*b)+(Math.sqrt(d)))/(2*a);
+            j = ((-1*b)-(Math.sqrt(d)))/(2*a);
+            System.out.println("Уравнение имеет два корня: "+i+" и "+j);
+        }
+        else
+            System.out.println("Уравнение корней не имеет из-за отриц. дискреминанта");
+    }
+}
