3da6f60aad427dbba9a0600cf5acc647727ea145
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-13 20:01:07 +0300

    Урок второй Домашнее задание №1

---
+++ src/second_homework1.java
@@ -0,0 +1,12 @@
+public class second_homework1 {
+    public static void main(String[] args) {
+        int n = 44;
+
+        if (n%2==0)
+            System.out.println("Число "+n+" является четным");
+        else
+            System.out.println("Число "+n+" является нечетным");
+
+
+    }
+}
