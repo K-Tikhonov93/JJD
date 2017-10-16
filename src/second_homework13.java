80b5bb5632539d363121e9fb4aadf1ddff76a115
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 15:28:44 +0300

    Урок второй Домашнее задание №13

---
+++ src/second_homework13.java
@@ -0,0 +1,19 @@
+import  java.util.*;
+public class second_homework13{
+    public static void main(String[] args){
+        int a;
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Введите натуральное число");
+        if(scn.hasNextInt()){
+            a=scn.nextInt();
+            System.out.print("Делителями числа "+a+" являются ");
+            for(int i=a;i>0;i--){
+                int b=a%i;
+                if(b==0)
+                    System.out.print(i+" ");
+            }
+        }
+        else System.out.println("Ошибка. Введено не число");
+
+    }
+}
