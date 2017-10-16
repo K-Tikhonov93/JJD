cc6d02e40a61170c5e7083e1f134e1e90a661b56
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 10:28:13 +0300

    Урок второй Домашнее задание №9

---
+++ src/second_homework9.java
@@ -0,0 +1,10 @@
+public class second_homework9 {
+    public static void main(String[] args) {
+        int a = 1;
+        for(int n=0;n<=55;n++){
+            a = a + 2;
+            System.out.println(a);
+        }
+
+    }
+}
