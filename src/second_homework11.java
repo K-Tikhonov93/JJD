b64124374b45e43af57dd54bf507ffa6a0af8623
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 10:38:20 +0300

    Урок второй Домашнее задание №11

---
+++ src/second_homework11.java
@@ -0,0 +1,10 @@
+public class second_homework11 {
+    public static void main(String[] args) {
+        int a = 1;
+        for(int n=0;n<=20;n++){
+            a = a * 2;
+            System.out.println(a);
+        }
+
+    }
+}
