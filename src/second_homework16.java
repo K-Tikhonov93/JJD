5cb301cc91bf215dc170e09e0a718fbc7b0ab8dc
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 15:37:18 +0300

    Урок второй Домашнее задание №3

---
+++ src/second_homework16.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class second_homework16 {
+    public static void main(String[] args){
+            int a,c=0;
+            Scanner scn=new Scanner(System.in);
+            if(scn.hasNextInt()){
+                a=scn.nextInt();
+                while(a!=0){
+                    c=c+a%10;
+                    a/=10;
+                }
+
+                System.out.println("Сумма всех чисел введенного числа равна "+c);
+            }
+            else System.out.println("Ошибка. Вы ввели не число!");
+        }
+    }
+
