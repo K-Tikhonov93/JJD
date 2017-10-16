b8de086707addb6b079edab8a3901fef3b86622d
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 18:52:18 +0300

    Урок второй Домашнее задание №3

--- src/second_homework4.java
+++ src/second_homework4.java
@@ -1,6 +1,6 @@
 public class second_homework4 {
     public static void main(String[] args) {
-        int a = (int) (Math.random()*150+1)+5;
+    int a = (int) (Math.random()*150+5);
         if (a<=25 || a>=100){
             System.out.println("Данное число "+a+" не попало в отрезок от 25 до 100");
         }
