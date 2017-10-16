11011df02fc1afd428840d1313c4dc731c4911c8
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 15:26:31 +0300

    Урок второй Домашнее задание №12

---
+++ src/second_homework12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class second_homework12{
+    public static void main(String[] args){
+
+        int a;
+        long b=1;
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Введите натуральное число");
+        if(scn.hasNextInt()){
+            a=scn.nextInt();
+            for(int i=a;i>0;i--){
+                b*=i;
+            }
+            System.out.println("Факториал числа "+a+" равен "+b);
+        }
+        else System.out.println("Ошибка. Введено не число");
+
+    }
+}
