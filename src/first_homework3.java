cff13bef664836544ae97fd940edda35000e315f
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 19:02:09 +0300

    Урок второй Домашнее задание №3

--- src/first_homework3.java
+++ src/first_homework3.java
@@ -4,9 +4,10 @@ public class first_homework3 {
         double b = 99.11;
 
         int x = (int)(a+0.5);
-        int y = (int)(b+0/5);
+        int y = (int)(b+0.5);
 
         System.out.println("Округлив число "+ a +" мы получим число "+ x);
         System.out.println("Округлив число "+ b +" мы получим число "+ y);
     }
 }
+
