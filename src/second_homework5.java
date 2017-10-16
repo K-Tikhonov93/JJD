67072cdb2e17a63942b32df3502c754619dbff2d
Konstantin Tikhonov <tikhonoff93@gmail.com>
2017-10-16 09:16:55 +0300

    Урок второй Домашнее задание №5

---
+++ src/second_homework5.java
@@ -0,0 +1,15 @@
+public class second_homework5 {
+    public static void main(String[] args) {
+        int a = (int) (Math.random()*1000);
+        System.out.println("рандомное число "+a);
+        int b = a%10;
+        int c = (a/10)%10;
+        int d = (a/100)%10;
+        if(b>=c&b>d||b>c&b>=d)
+            System.out.println("В числе "+a+" наибольшее число "+b);
+        else if (c>b&c>=d)
+            System.out.println("В числе "+a+" наибольшее число "+c);
+        else
+            System.out.println("В числе "+a+" наибольшее число "+d);
+    }
+}
